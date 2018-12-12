c2f8137309fbe699987bf67ea3edbddff860e742
Katie Gately <kaitlin_gately@brown.edu>
2018-12-12 12:46:10 -0500

    All functions added

--- EpiVision/R/package code.R
+++ EpiVision/R/package code.R
@@ -32,6 +32,8 @@ library(rprev)
 library(ggmap)
 library(mapdata)
 library(viridis)
+library(RColorBrewer)
+library(plotly)
 
 
 #Load sample dataset available in the AER package
@@ -39,10 +41,8 @@ library(viridis)
 library(AER)
 data(Fatalities)
 
-#NOTE: no data for Alaska or Hawaii
 
 #Cleaning the state variable data
-
 us_map <- map_data("state")
 
 tbl <- state.x77 %>%
@@ -169,4 +169,69 @@ map_west_prevalence <- function(data,existing_cases,population,state,year) {
 }
 
 map_west_prevalence(Fatalities_clean,existing_cases= "fatal",population="pop", state="state_full", year="year")
+
+#Function for plotting
+epiplot<- function(data, x, y, graph, fill, title, xlab, ylab, legend){    ###add argument for error bars
+  data[complete.cases(data), ]
+  if(graph == "bar"){                           #boxplot function
+    pic<-  ggplot(data=data, aes(x=x, fill=x)) +
+      geom_bar( ) +
+      scale_fill_brewer(palette = "Paired")+
+      labs(title="title", x="xlab", y="ylab")
+    #table1 = table(data$x)  ## get the cross tab
+    #pic<-barplot(table1, beside = TRUE, legend = levels(data$x), col=c("lightblue","darkblue"),main="title", xlab="xlab", ylab = "ylab")
+    return(pic)
+    #barplot(table(data$x), col=c("lightblue","darkblue"),main="title", xlab="xlab", ylab = "ylab")
+  } else if(graph=="bargroup"){                                           #works, but has column for NAs....why???
+    pic<-ggplot(data=data, aes(x=x, y=y, fill=fill)) +
+      geom_bar(position="dodge", stat="identity") + scale_fill_brewer(palette = "Paired")+theme_bw()+facet_wrap(~"fill")
+    return(pic)
+  } else if(graph=="barstack"){                                           #works, but has column for NAs....why???
+    pic<-ggplot(data=data, aes(fill=fill, y=y, x=x)) +
+      geom_bar( stat="identity")
+    return(pic)
+  } else if(graph=="boxplot"){                                            #GOOD
+    pic<-boxplot(y~x, data=data, notch=TRUE,
+                 main="title", xlab="xlab", ylab="ylab")
+    return(pic)
+  } else if(graph=="dotboxplot"){                                         #GOOD
+    pic<-plot_ly(y = ~y, type = "box", boxpoints = "all", jitter = 0.3,pointpos = -1.8)
+    return(pic)
+  } else if(graph=="hist"){                                               #GOOD
+    pic<-ggplot(data=data, aes(x)) +
+      geom_histogram(col="black", aes(fill=..count..)) +
+      scale_fill_gradient("Count", low="light blue", high="navy")+
+      labs(title="title", x="xlab", y="ylab")
+    return(pic)
+  } else if(graph=="densityhist"){                                          #GOOD
+    pic<-ggplot(data=data, aes(x)) +
+      geom_histogram(aes(y =..density..),col="blue", fill="light blue", alpha=.5) +
+      geom_density(col=2) +
+      labs(title="title", x="xlab", y="ylab")
+    return(pic)
+  } else if(graph=="scatter"){                                            #GOOD
+    pic<-ggplot(data, aes(x, y, color = fill)) +
+      geom_point(shape = 16, size = 5, show.legend = TRUE) +
+      theme_minimal() +
+      #scale_color_gradient(color = "Blues")+
+      labs(title="title", x="xlab", y="ylab", color = "legend")
+    return(pic)
+  } else if(graph=="scatterline"){                                        #GOOD
+    pic<-ggplot(data, aes(x, y, color = fill)) +
+      geom_point(shape = 16, size = 5, show.legend = TRUE) +
+      theme_minimal() +
+      scale_color_gradient(low = "light blue", high = "dark blue")+
+      labs(title="title", x="xlab", y="ylab", color = "legend")+geom_smooth()
+    return(pic)
+  } else if(graph=="linreg"){                                             #GOOD
+    pic<-ggplot(data, aes(x, y, color = fill)) +
+      geom_point(shape = 16, size = 5, show.legend = TRUE) +
+      theme_minimal() +
+      scale_color_gradient(low = "light blue", high = "dark blue")+
+      labs(title="title", x="xlab", y="ylab", color = "legend")+ geom_smooth(method = 'lm', se = TRUE)
+    return(pic)
+  }
+}
+
+
 
