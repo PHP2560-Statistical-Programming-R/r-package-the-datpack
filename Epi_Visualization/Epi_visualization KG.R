f76f7abe71769c9868b5046891affc09b85d71a0
Katie Gately <kaitlin_gately@brown.edu>
2018-12-12 09:58:17 -0500

    Uploaded dataset

--- Epi_Visualization/Epi_visualization KG.R
+++ Epi_Visualization/Epi_visualization KG.R
@@ -9,43 +9,22 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
+#Making Cleaned dataset into a CSV file
+exported_data<-write.table(Fatalities_clean, file="Fatalities_clean",sep=",",row.names=F)
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
-}
+test_dataset_fatalities <- read.csv("Fatalities_clean.csv", stringsAsFactors = FALSE)
 
 
-# Run the application 
+ui <- fluidPage()
+server <- function(input, output) {}
 shinyApp(ui = ui, server = server)
+
+      
+      
+      
+      
+      
+      
+  
+
 
