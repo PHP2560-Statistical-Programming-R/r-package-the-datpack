42bcd6d622920ac22329b9c3ec2762d1ab206303
Arielle Childs <arielle_childs@brown.edu>
2018-12-12 11:18:57 -0500

    Set up tabs

---
+++ Epi_Visualization/Epi_visualization ARC.R
@@ -0,0 +1,87 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+
+#Making Cleaned dataset into a CSV file
+#exported_data<-write.table(Fatalities_clean, file="Fatalities_clean.csv",sep=",",row.names=F)
+
+#test_dataset_fatalities <- read.csv("Fatalities_clean.csv", stringsAsFactors = FALSE)
+
+ui <- pageWithSidebar(
+  
+  # App title: Epi Visualization
+  headerPanel("Epi Visualization"),
+  
+  # Sidebar panel for inputs ----
+  sidebarPanel(
+    helpText("Create demographic maps and plot with 
+             information from the US Traffic Fatalities Panel Data"),
+    # Input: Selector for variable to plot against mpg ----
+    selectInput("variable", 
+                label = "Choose a variable to display",
+                choices = c("Fatalities" = "Fatal",
+                            "State" = "state_full",
+                            "Income" = "income",
+                            "Year" = "year")),
+    
+    sliderInput(inputId = "year",
+                label = "Years:",
+                min = 1982,
+                max = 1988,
+                value = 1),
+    
+    # Input: Checkbox for whether outliers should be included ----
+    checkboxInput("outliers", "Show outliers", TRUE)
+    
+    ),
+  
+  # Main panel for displaying outputs ----
+  mainPanel(
+    h4("This app provides tools to help visualize epidemiologic data"),
+    h5("The mapping functions allow users to visualize the prevalence of their variable of interest at the national and regional levels. 
+       The graph functions allow users to plot and visualize their data in many ways?? Mention features."), 
+    tags$head(
+         tags$style(type='text/css', 
+                    ".nav-tabs {font-size: 14px} ")), 
+    tabsetPanel(type = "tabs", 
+                tabPanel("Scatterplots", plotOutput("ScatterMatrix", width = "100%", height = "580px"),
+                         textInput("text_scatt", label = "Interpretation", value = "Enter text...")), 
+                tabPanel("Boxplots", plotOutput("BoxPlot", height = "580px"),
+                         textInput("text_box", label = "Interpretation", value = "Enter text...")),
+                tabPanel("Summary statistics", br(),verbatimTextOutput("lmResults"),
+                         textInput("text_summary", label = "Interpretation", value = "Enter text...")), 
+                tabPanel("Diagnostic plots",  plotOutput("diagnostics", height = "580px"),
+                         textInput("text_diagno", label = "Interpretation", value = "Enter text...")),
+                tabPanel("Added variable plots",  plotOutput("added", height = "580px"),
+                         textInput("text_added", label = "Interpretation", value = "Enter text...")),
+                tabPanel("Marginal model plots",  plotOutput("MMPlot", height = "580px"),
+                         textInput("text_mmp", label = "Interpretation", value = "Enter text...")),
+                tabPanel("Help",  htmlOutput("inc"))
+                )
+    )
+  )
+
+server <- function(input, output) {
+  
+}
+
+
+shinyApp(ui, server)
+
+
+
+
+
+#ui <- fluidPage() (basic structure of the app)
+#server <- function(input, output) {}
+#shinyApp(ui = ui, server = server)
+
+
