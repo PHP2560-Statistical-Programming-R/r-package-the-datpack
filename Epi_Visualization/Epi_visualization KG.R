ca40a072aa23e570741609501e1cbc695fb9bde0
Arielle Childs <arielle_childs@brown.edu>
2018-12-12 11:24:32 -0500

    Katie's code

--- Epi_Visualization/Epi_visualization KG.R
+++ Epi_Visualization/Epi_visualization KG.R
@@ -19,59 +19,84 @@ ui <- pageWithSidebar(
   
   # App title: Epi Visualization
   headerPanel("Epi Visualization"),
-
+  
   # Sidebar panel for inputs ----
   sidebarPanel(
     helpText("Create demographic maps and plot with 
-               information from the US Traffic Fatalities Panel Data"),
-  # Input: Selector for variable to plot against mpg ----
-  selectInput("variable", 
-              label = "Choose a variable to display",
-              choices = c("Fatalities" = "Fatal",
-                  "State" = "state_full",
-                 "Income" = "income",
-                  "Year" = "year")),
-             
-  sliderInput(inputId = "year",
-              label = "Years:",
-              min = 1982,
-              max = 1988,
-              value = 1),
-  
-  # Input: Checkbox for whether outliers should be included ----
-  checkboxInput("outliers", "Show outliers", TRUE)
-  
-),
+             information from the US Traffic Fatalities Panel Data"),
+    # Input: Selector for variable to plot against mpg ----
+    selectInput("variable 1", 
+                label = "Choose a variable to display",
+                choices = c("Fatalities" = "Fatal",
+                            "State" = "state_full",
+                            "Income" = "income",
+                            "Year" = "year")),
+    selectInput("variable 1", "Choose a variable to display", 
+                choices = c("None" = "NA", "Fatalities" = "Fatal",
+                            "State" = "state_full",
+                            "Income" = "income",
+                            "Year" = "year")),
+    selectInput("variable 3", "Choose a variable to display", 
+                choices = c("None" = "NA","Fatalities" = "Fatal",
+                            "State" = "state_full",
+                            "Income" = "income",
+                            "Year" = "year")),
+    
+    sliderInput(inputId = "year",
+                label = "Years:",
+                min = 1982,
+                max = 1988,
+                value = 1),
+    
+    # Input: Checkbox for whether outliers should be included ----
+    checkboxInput("outliers", "Show outliers", TRUE)
+    
+    ),
   
   # Main panel for displaying outputs ----
   mainPanel(
     h4("This app provides tools to help visualize epidemiologic data"),
-    h5("The mapping functions allow users to visualize the prevalence of their variable of interest at the national and regional levels. 
-       The graph functions allow users to plot and visualize their data in many ways?? Mention features.")
-  )
+    h5("The mapping functions allow users to visualize the prevalence of their variable of 
+       interest at the national and regional levels. The graph functions allow users to plot 
+       and visualize their data in many ways?? Mention features."),
+    tags$head(
+      tags$style(type='text/css', 
+                 ".nav-tabs {font-size: 14px} ")), 
+    tabsetPanel(type = "tabs", 
+                tabPanel("Exploratory Data Analysis", plotOutput("ScatterMatrix", width = "100%", 
+                                                                 height = "580px"),
+                         textInput("text_scatt", label = "Interpretation", value = "Enter text...")), 
+                tabPanel("Basic Graphing", plotOutput("BoxPlot", height = "580px"),
+                         textInput("text_box", label = "Interpretation", value = "Enter text...")),
+                tabPanel("Epi Tools", br(),verbatimTextOutput("lmResults"),
+                         textInput("text_summary", label = "Interpretation", value = "Enter text...")),
+                tabPanel("National Map",  plotOutput("added", height = "580px"),
+                         textInput("text_added", label = "Interpretation", value = "Enter text...")),
+                tabPanel("Regional Maps",  plotOutput("MMPlot", height = "580px"),
+                         textInput("text_mmp", label = "Interpretation", value = "Enter text...")),
+                tabPanel("Help",  htmlOutput("inc"))
+    )
+    )
 )
 
 server <- function(input, output) {
- 
-}
   
+}
+
 
 shinyApp(ui, server)
 
+#Things to Figure out:
+#  1. Upload dataset
+#  2. What tabs to include
+#  3. Functions
 
 
 
 
-#ui <- fluidPage() (basic structure of the app)
-#server <- function(input, output) {}
-#shinyApp(ui = ui, server = server)
 
-      
-      
-      
-      
-      
-      
-  
+
+
+
 
 
